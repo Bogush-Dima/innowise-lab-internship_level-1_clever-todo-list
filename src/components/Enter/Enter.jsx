ee094e68a49b2d222232ffd02bd04be3d6f7bcdf
Bogush-Dima <bogusd66@gmail.com>
2021-04-01 16:50:49 +0300

    create authorization

--- src/components/Enter/Enter.jsx
+++ src/components/Enter/Enter.jsx
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { fireAuth } from 'utils/database';
 import { StyledWrapper, StyledForm, StyledInput } from './Styled';
 
-export const Enter = ({ method }) => (
-  <StyledWrapper>
-    <StyledForm>
-      <StyledInput type="email" placeholder="email" />
-      <StyledInput type="password" placeholder="password" />
-      <button type="button">{method === 'signUp' ? 'Sign Up' : 'Sign In'}</button>
-    </StyledForm>
-  </StyledWrapper>
-);
+export class Enter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '' };
+  }
+
+  render() {
+    const { method } = this.props;
+    const { email, password } = this.state;
+
+    const changeEmail = (event) => {
+      event.preventDefault();
+      this.setState({ email: event.target.value });
+    };
+
+    const changePassword = (event) => {
+      event.preventDefault();
+      this.setState({ password: event.target.value });
+    };
+
+    const submit = (event) => {
+      event.preventDefault();
+
+      if (method === 'signUp') {
+        fireAuth
+          .createUserWithEmailAndPassword(email, password)
+          .then(() => {
+            window.location.pathname = '/todolist';
+          })
+          // eslint-disable-next-line no-console
+          .catch((error) => console.log(error.message));
+      } else {
+        fireAuth
+          .signInWithEmailAndPassword(email, password)
+          .then(() => {
+            window.location.pathname = '/todolist';
+          })
+          // eslint-disable-next-line no-console
+          .catch((error) => console.log(error.message));
+      }
+    };
+
+    return (
+      <StyledWrapper>
+        <StyledForm>
+          <StyledInput type="email" onChange={changeEmail} value={email} placeholder="email" />
+          <StyledInput
+            type="password"
+            onChange={changePassword}
+            value={password}
+            placeholder="password"
+          />
+          <button onClick={submit} type="submit">
+            {method === 'signUp' ? 'Sign Up' : 'Sign In'}
+          </button>
+        </StyledForm>
+      </StyledWrapper>
+    );
+  }
+}
 
 Enter.propTypes = {
   method: PropTypes.string.isRequired,
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,65 @@
 import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
+import { fireAuth } from 'utils/database';
 import { StyledHeader } from './Styled';
 
-export const Header = () => (
-  <Switch>
-    <StyledHeader>
-      <Route
-        path="/signIn"
-        render={() => (
-          <Link to="signUp">
-            <button type="button">Sign Up</button>
-          </Link>
-        )}
-      />
-      <Route
-        path="/signUp"
-        render={() => (
-          <Link to="signIn">
-            <button type="button">Sign In</button>
-          </Link>
-        )}
-      />
-    </StyledHeader>
-  </Switch>
-);
+export const Header = () => {
+  const signOut = (event) => {
+    event.preventDefault();
+    fireAuth.signOut();
+    window.location.pathname = '/signIn';
+  };
+
+  return (
+    <Switch>
+      <StyledHeader>
+        <Route
+          path="/signIn"
+          render={() => (
+            <Link to="signUp">
+              <button type="button">Sign Up</button>
+            </Link>
+          )}
+        />
+        <Route
+          path="/signUp"
+          render={() => (
+            <Link to="signIn">
+              <button type="button">Sign In</button>
+            </Link>
+          )}
+        />
+        <Route
+          path="/todolist"
+          render={() => (
+            <>
+              <Link to="createTodos">
+                <button type="button">Create Todos</button>
+              </Link>
+              <Link to="signIn">
+                <button onClick={signOut} type="button">
+                  Sign Out
+                </button>
+              </Link>
+            </>
+          )}
+        />
+        <Route
+          path="/createTodos"
+          render={() => (
+            <>
+              <Link to="todolist">
+                <button type="button">Todo List</button>
+              </Link>
+              <Link to="signIn">
+                <button onClick={signOut} type="button">
+                  Sign Out
+                </button>
+              </Link>
+            </>
+          )}
+        />
+      </StyledHeader>
+    </Switch>
+  );
+};
