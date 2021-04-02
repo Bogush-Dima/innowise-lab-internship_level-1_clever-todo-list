299a3aae076f5b029415dd16dbe73d609bbc2077
Bogush-Dima <bogusd66@gmail.com>
2021-04-02 11:09:26 +0300

    create adding and getting todos

--- src/components/CreateTodos/CreateTodos.jsx
+++ src/components/CreateTodos/CreateTodos.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { fireAuth, fireDB } from 'utils/database';
 import {
   StyledSection,
   StyledForm,
@@ -8,26 +9,76 @@ import {
   StyledUl,
 } from './Styled';
 
-export const CreateTodos = () => {
-  const fun = (event) => {
-    event.preventDefault();
-  };
+export class CreateTodos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todoName: '', todoDescription: '', date: '', todoList: {} };
+  }
 
-  return (
-    <StyledSection>
-      <StyledUl>
-        <StyledTitle>Todo List</StyledTitle>
-        <li>Todo</li>
-        <li>Todo</li>
-        <li>Todo</li>
-      </StyledUl>
-      <StyledForm onSubmit={fun}>
-        <StyledTitle>Create New Todo</StyledTitle>
-        <StyledInput type="text" placeholder="Todo Name" />
-        <StyledTextarea placeholder="Todo Description" />
-        <StyledInput type="date" />
-        <input type="submit" value="Save" />
-      </StyledForm>
-    </StyledSection>
-  );
-};
+  render() {
+    const { todoName, todoDescription, date, todoList } = this.state;
+
+    const getTodos = () => {
+      fireDB.ref(`/${fireAuth.currentUser.email.replace('.', '_')}`).on('value', (snapShot) => {
+        const resObj = {};
+        snapShot.forEach((childSnapshot) => {
+          const { key } = childSnapshot;
+          const objects = childSnapshot.val();
+          const entr = Object.entries(objects);
+          const objArr = entr.map((arr) => {
+            const key1 = arr[0];
+            const obj = arr[1];
+            const res = { key: key1, ...obj };
+            return res;
+          });
+          resObj[key] = objArr;
+        });
+        this.setState({ todoList: resObj });
+      });
+    };
+
+    const submit = async (event) => {
+      event.preventDefault();
+      await fireDB.ref(`/${fireAuth.currentUser.email.replace('.', '_')}/${date}`).push({
+        date,
+        todoName,
+        todoDescription,
+        done: false,
+      });
+      getTodos();
+    };
+
+    const changeTodoName = (event) => {
+      event.preventDefault();
+      this.setState({ todoName: event.target.value });
+    };
+
+    const changeTodoDescription = (event) => {
+      event.preventDefault();
+      this.setState({ todoDescription: event.target.value });
+    };
+
+    const changeDate = (event) => {
+      event.preventDefault();
+      this.setState({ date: event.target.value });
+    };
+
+    return (
+      <StyledSection>
+        <StyledUl>
+          <StyledTitle>Todo List</StyledTitle>
+          <li>Todo</li>
+          <li>Todo</li>
+          <li>Todo</li>
+        </StyledUl>
+        <StyledForm onSubmit={submit}>
+          <StyledTitle>Create New Todo</StyledTitle>
+          <StyledInput onChange={changeTodoName} type="text" placeholder="Todo Name" />
+          <StyledTextarea onChange={changeTodoDescription} placeholder="Todo Description" />
+          <StyledInput onChange={changeDate} type="date" />
+          <input type="submit" value="Save" />
+        </StyledForm>
+      </StyledSection>
+    );
+  }
+}
