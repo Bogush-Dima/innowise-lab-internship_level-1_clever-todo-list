b30b2c42fe2fed34dcc11c016eedab09c549102b
Bogush-Dima <bogusd66@gmail.com>
2021-04-15 15:23:56 +0300

    weak refactor Calendar

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -1,1 +1,9 @@
 export const changeUserEmail = (user) => user.email.replace('.', '_');
+
+export const formattingDateStrForFirebase = (date = new Date()) => {
+  const dateStr = date.toLocaleDateString('en-us');
+  const arr = dateStr.split('/');
+  const newArr = arr.map((num) => (+num < 10 ? `0${num}` : num));
+  const res = `${newArr[2]}-${newArr[0]}-${newArr[1]}`;
+  return res;
+};
